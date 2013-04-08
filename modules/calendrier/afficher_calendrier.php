42fdf62a093555a543e806c43fee562e89b8977e
alexisprunier <prunier.al@gmail.com>
2013-04-08 22:02:13 +0200

    avancement dans le calendrier

--- modules/calendrier/afficher_calendrier.php
+++ modules/calendrier/afficher_calendrier.php
@@ -1,4 +1,30 @@
 <?php
+include CHEMIN_MODELE . 'evenement.php';
+include CHEMIN_MODELE . 'personne.php';
+include CHEMIN_MODELE . 'dossier.php';
+
+$tab_evenement = lister_evenement_dans_bdd();
+
+$lignes_tableau = array();
+
+foreach ($tab_evenement as &$evenement){
+    
+    if ($evenement->user_id == $_SESSION['id']){
+        $dossier = selectionner_dossier_dans_bdd($evenement->dossier_id);
+        $personne = selectionner_personne_dans_bdd($dossier['personne_id']);
+        
+        $ligne = array(
+            'n_dossier' => $dossier['dossier_ref'],
+            'nom' => $personne['nom'],
+            'prenom' => $personne['prenom'],
+            'mode_contact' => $evenement->mode_contact,
+            'date' => $evenement->date_event,
+        );
+
+        array_push($lignes_tableau, $ligne);
+        
+    }
+}
 
 include CHEMIN_VUE . 'vue_calendrier.php';
 ?>
--- modules/calendrier/vues/vue_calendrier.php
+++ modules/calendrier/vues/vue_calendrier.php
@@ -1,1 +1,56 @@
-<div id='calendar'></div>
+<script type="text/javascript">
+   
+var lignes_tab = <?php echo json_encode($lignes_tableau) ?>;
+var event = []
+
+for (var i = 0; i < lignes_tab.lenght; i++){
+    var new_dict = {
+        title: 'oui oui',
+        start: new Date(
+            lignes_tab['date'].substring(0, 2),
+            lignes_tab['date'].substring(3, 2),
+            lignes_tab['date'].substring(6, 2)),
+    }
+    event[i] = new_dict;
+}
+
+$(document).ready(function() { 
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        $('#calendar').fullCalendar({
+                header: {
+                        center: '',
+                        left: 'title',
+                        right: 'prev,next month,basicWeek,basicDay'
+                },
+                editable: false,
+
+                events: event
+                        
+               /*[
+                        {
+                                title: 'loliklol',
+                                start: new Date(y, m, 1)
+
+
+                        },
+                        {
+                                title: 'Long Event',
+                                start: new Date(y, m, d-5),
+                                end: new Date(y, m, d-2)
+                        },
+                        {
+                                id: 999,
+                                title: 'Repeating Event',
+                                start: new Date(y, m, d-3, 16, 0),
+                                allDay: false
+                        }
+                ]*/
+        });
+});	
+</script>
+<div id='calendar'>  
+</div>
