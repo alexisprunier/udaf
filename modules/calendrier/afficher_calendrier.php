4877465b72b3e10706f30befef1ac69ce1630e60
alexis_prunier <alexis_prunier@bitbucket.org>
2013-04-09 21:41:07 +0200

    finish calendar

--- modules/calendrier/afficher_calendrier.php
+++ modules/calendrier/afficher_calendrier.php
@@ -2,23 +2,28 @@
 include CHEMIN_MODELE . 'evenement.php';
 include CHEMIN_MODELE . 'personne.php';
 include CHEMIN_MODELE . 'dossier.php';
+include CHEMIN_MODELE . 'utilisateur.php';
 
 $tab_evenement = lister_evenement_dans_bdd();
 
 $lignes_tableau = array();
 
 foreach ($tab_evenement as &$evenement){
-    print 'x';
+
     if ($evenement->user_id == $_SESSION['id']){
         $dossier = selectionner_dossier_dans_bdd($evenement->dossier_id);
         $personne = selectionner_personne_dans_bdd($dossier['personne_id']);
         
         $ligne = array(
-            'n_dossier' => $dossier['dossier_ref'],
+            /*'n_dossier' => $dossier['dossier_ref'],
             'nom' => $personne['nom'],
             'prenom' => $personne['prenom'],
-            'mode_contact' => $evenement->mode_contact,
-            'date' => $evenement->date_event,
+            'mode_contact' => $evenement->mode_contact,*/
+            
+            'title' => 'coucou',
+            'start' => $evenement->date_event,
+            'end' => $evenement->date_event,
+            'allDay' => false
         );
 
         array_push($lignes_tableau, $ligne);
