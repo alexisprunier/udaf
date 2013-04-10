1a85d988781af05b049e6c103cca4aa0e72b9cd3
alexis_prunier <alexis_prunier@bitbucket.org>
2013-04-10 10:39:31 +0200

    supprimer commentaires

--- modules/calendrier/vues/vue_calendrier.php
+++ modules/calendrier/vues/vue_calendrier.php
@@ -22,27 +22,18 @@ function feed(){
     $lignes_tableau = array();
 
     foreach ($tab_evenement as &$evenement){
+        $dossier = selectionner_dossier_dans_bdd($evenement->dossier_id);
+        $personne = selectionner_personne_dans_bdd($dossier['personne_id']);
+        $utilisateur = selectionner_utilisateur_dans_bdd($evenement->user_id);
 
-        //if ($evenement->user_id == $_SESSION['id']){
-            $dossier = selectionner_dossier_dans_bdd($evenement->dossier_id);
-            $personne = selectionner_personne_dans_bdd($dossier['personne_id']);
-            $utilisateur = selectionner_utilisateur_dans_bdd($evenement->user_id);
+        $ligne = array(
+            'title' => 'Contact de '.$utilisateur['ident'].' avec '.$personne['nom'].' '.$personne['prenom'].' par '.$evenement->mode_contact,
+            'start' => substr($evenement->date_event, 6, 4)."-".substr($evenement->date_event, 3, 2)."-".substr($evenement->date_event, 0, 2)."T00:00:00Z",
+            'end' => substr($evenement->date_event, 6, 4)."-".substr($evenement->date_event, 3, 2)."-".substr($evenement->date_event, 0, 2)."T00:00:00Z",
+            'allDay' => false
+        );
 
-            $ligne = array(
-                /*'n_dossier' => $dossier['dossier_ref'],
-                'nom' => $personne['nom'],
-                'prenom' => $personne['prenom'],
-                'mode_contact' => $evenement->mode_contact,*/
-
-                'title' => 'Contact de '.$utilisateur['ident'].' avec '.$personne['nom'].' '.$personne['prenom'].' par '.$evenement->mode_contact,
-                'start' => substr($evenement->date_event, 6, 4)."-".substr($evenement->date_event, 3, 2)."-".substr($evenement->date_event, 0, 2)."T00:00:00Z",
-                'end' => substr($evenement->date_event, 6, 4)."-".substr($evenement->date_event, 3, 2)."-".substr($evenement->date_event, 0, 2)."T00:00:00Z",
-                'allDay' => false
-            );
-
-            array_push($lignes_tableau, $ligne);
-
-        //}
+        array_push($lignes_tableau, $ligne);
     }
     echo json_encode($lignes_tableau);
 }
