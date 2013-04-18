e332d873224c0d880167ad7a4f7aecd0c2f5cb26
alexis_prunier <alexis_prunier@bitbucket.org>
2013-04-18 21:57:04 +0200

    avancer dans les stats (reste une correction a faire)

--- modules/statistique/recuperer_stats.php
+++ modules/statistique/recuperer_stats.php
@@ -8,24 +8,37 @@
  */
 
 include CHEMIN_MODELE . 'evenement.php';
+include CHEMIN_MODELE . 'dossier.php';
 
 $tab_evenement = lister_evenement_dans_bdd();
 
 $year = date(Y);
+$tableau = array();
 
-foreach ($$tab_evenement as &$event){
+foreach ($tab_evenement as &$event){
     if ( substr($event->dossier_id, 0, 4) == $year ) {
-        
+        $date_existe = false;
+        for ( $i=0; $i<count($tableau); $i++ ) {
+            echo $tableau[i]['date'];
+            echo $event->date_event.'+++';
+            if ( $tableau[i][date] == $event->date_event ){
+                $tableau[i][$event->mode_contact] += 1;
+                $tableau[i]['total'] += 1;
+                $tableau[i][selectionner_dossier_dans_bdd($event->dossier_id)->theme_id] += 1;
+                $date_existe = true;
+            }
+        }
+        if ( !$date_existe ){
+            $dossier = selectionner_dossier_dans_bdd($event->dossier_id);
+            $nouv_ligne = array(
+                'date' => $event->date_event,
+                $event->mode_contact => 1,
+                'total' => 1,
+                $dossier['theme_id'] => 1);
+            $tableau[count($tableau)] = $nouv_ligne;
+        }
     }
 }
-
-
-
-
-
-
-
-
 
 include CHEMIN_VUE . 'vue_statistique.php';
 ?>
--- modules/statistique/vues/vue_statistique.php
+++ modules/statistique/vues/vue_statistique.php
@@ -37,6 +37,38 @@
             </tr>
         </thead>
         <tbody>
+            <?php  
+            for ( $i=0 ; $i<count($tableau) ; $i++ ) {
+                $ligne = $tableau[$i];
+            ?>
+                <tr>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['date']!=null ? utf8_encode($ligne['date']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['rdv']!=null ? utf8_encode($ligne['rdv']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['tel']!=null ? utf8_encode($ligne['tel']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['mail']!=null ? utf8_encode($ligne['mail']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['total']!=null ? utf8_encode($ligne['total']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['1']!=null ? utf8_encode($ligne['1']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['2']!=null ? utf8_encode($ligne['2']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['3']!=null ? utf8_encode($ligne['3']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['4']!=null ? utf8_encode($ligne['4']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['5']!=null ? utf8_encode($ligne['5']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['6']!=null ? utf8_encode($ligne['6']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['7']!=null ? utf8_encode($ligne['7']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['8']!=null ? utf8_encode($ligne['8']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['9']!=null ? utf8_encode($ligne['9']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['10']!=null ? utf8_encode($ligne['10']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['11']!=null ? utf8_encode($ligne['11']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['12']!=null ? utf8_encode($ligne['12']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['13']!=null ? utf8_encode($ligne['13']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['14']!=null ? utf8_encode($ligne['14']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['15']!=null ? utf8_encode($ligne['15']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['16']!=null ? utf8_encode($ligne['16']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['17']!=null ? utf8_encode($ligne['17']) : '0' ); ?></td>
+                    <td  class="cell_result" align="center"><?php echo ($ligne['18']!=null ? utf8_encode($ligne['18']) : '0' ); ?></td>
+                </tr>
+            <?php
+            }
+            ?>
         </tbody>
     </table>
 </div>
