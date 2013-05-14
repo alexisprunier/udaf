96f054d4d8b3bf6ec159305c02844fb5d4f24fba
tonyodelaliverda <tonyodelaliverda@bitbucket.org>
2013-05-14 16:24:41 +0200

    Redirection vers le dossier visualisé

--- modules/dossier/vues/formulaire_ajouter_fichier.php
+++ modules/dossier/vues/formulaire_ajouter_fichier.php
@@ -5,7 +5,7 @@
 
 <div id="ajouter_fichier">
   <h3> Ajouter fichier au dossier</h3>
-  <form name = "form_fichier" action="accueil.php?module=dossier&action=creer_dossier&ajout=fichiers" onsubmit="return valider_nom(this)" method="post" enctype="multipart/form-data">
+  <form name = "form_fichier" action="accueil.php?module=dossier&action=creer_dossier&ajout=fichiers&id=<?php echo $_SESSION['dossier_ref']; ?>" onsubmit="return valider_nom(this)" method="post" enctype="multipart/form-data">
     <fieldset class="ogconso">
         <input type="hidden" name="MAX_FILE_SIZE" value="5000000"> <!-- 5Mo-->
         <input id="fichier" name="fichier" type="file" required onchange="verifier_fichier(this)"/><br />
--- modules/dossier/vues/formulaire_creation_site.php
+++ modules/dossier/vues/formulaire_creation_site.php
@@ -2,7 +2,7 @@
 <div id="ajouter_site">
     <h3> Ajouter site web au dossier</h3>
 
-        <form id="form_creer_dossier" action="accueil.php?module=dossier&action=creer_dossier&ajout=site" method="post" enctype="multipart/form-data">
+        <form id="form_creer_dossier" action="accueil.php?module=dossier&action=creer_dossier&ajout=site&id=<?php echo $_SESSION['dossier_ref']; ?>"  method="post" enctype="multipart/form-data">
             <fieldset class="ogconso">
                 <label for="name" class="lab_txt">Nom du site : </label>
                 <input type="text" class="inputfield" name="name" id="name" required title="Celui qui apparaitra dans le tableau" />
