b0028a93fba1ad63f62f80dcc289e826674168a6
tonyodelaliverda <tonyodelaliverda@bitbucket.org>
2013-05-24 20:23:08 +0200

    Debut DOC a ajouter image

--- modules/documentation/vues/vue_documentation.php
+++ modules/documentation/vues/vue_documentation.php
@@ -33,10 +33,7 @@
     <ul id="menu_doc">
         <li class='titre_menu_doc'>Gérer un client</li>
         <ul class="ssmenu_doc">
-            <li><a href="ajouter_client">Ajouter un client</a></li>
             <li><a href="modifier_client">Modifier un client</a></li>
-            <li><a href="consulter_client">Consulter un client</a></li>
-            <li><a href="transferer_client">Transférer un client à un autre utilisateur</a></li>
             <?php
             if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
             ?>
@@ -51,7 +48,7 @@
         </ul>
         <li class='titre_menu_doc'>Gérer les fournisseurs</li>
         <ul class="ssmenu_doc">
-            <li><a href="ajouter_fournisseur">Ajouter un fournisseur au dossier</a></li>
+            <li><a href="ajouter_fournisseur">Modifier fournisseur du dossier</a></li>
              <?php
             if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
             ?>
@@ -67,9 +64,10 @@
         <li class='titre_menu_doc'>Gerer les dossier</li>
         <ul class="ssmenu_doc">
             <li><a href="creer_dossier">Creer un nouveau dossier</a></li>
-            <li><a href="suivi_dossier">Consulter/Modifier les informations d'un de ses dossier</a></li>
+            <li><a href="suivi_dossier">Modifier les informations d'un de ses dossier</a></li>
             <li><a href="rechercher_dossier">Consulter les information d'un dossier</a></li>
             <li><a href="exporter_dossier">Exporter une fiche de dossier vierge</a></li>
+            <li><a href="transferer_client">Transférer un dossier à un autre utilisateur</a></li>
 
         </ul>
         <li class='titre_menu_doc'>Générer un rapport annuel</li>
@@ -96,10 +94,32 @@
  
 <div id="contenu_doc">
     <!--DOC GERER UN CLIENT-->
-    <div id="doc_ajouter_client" class="contenu_doc">CONTENU PAGE AJOUTER CLIENT</div>
-    <div id="doc_modifier_client" class="contenu_doc">CONTENU PAGE MODIFIER CLIENT</div>
-    <div id="doc_consulter_client" class="contenu_doc">CONTENU PAGE CONSULTER CLIENT</div>
-    <div id="doc_transferer_client" class="contenu_doc">CONTENU PAGE TRANSFERER UN CLIENT</div>
+   <div id="doc_suivi_dossier" class="contenu_doc">
+        <p>
+        La modification d'un dossier est possible si et seulement si vous êtes proprietaire du dossier. Les dossiers dont vous êtes le propriétaire se trouve dans l'espace "Suivi Dossiers"
+        .</p> 
+        <p>Pour modifier un dossier cliquez sur l'élément du menu "Suivi Dossiers" :<br/>
+            Vous pouvez dans cette page filtrer les dossier a afficher. Pour cela remplissez les quelques champs afin d'optimiser le temps de recherche du dossier désiré</p>
+        <p>Lorsque le dossier désiré est affiché cliquez sur la ligne, vous êtes redirigé vers la page créer dossier ou toute les info sont remplient.</p>
+        <p>Dans cet espace on trouve deux fonctionnement :<br>
+        Le premier, les champs d'information client, fournisseur, theme, sous theme, problématique, l'espace cloture et l'espace proprietaire.<br>
+        Cela signifie que lorsque l'une ou plusieurs de ces informations sont modifiées il faudra cliquer sur le bouton "Modifier dossier" afin que les données modifier soit enregistrées.<br>
+        <br>
+        Le deuxieme, les espace gestion fichiers, gestion sites et gestion des événements quant à eux ne necessite pas de cliquer sur le bouton "Modifier le dossier" en effet ces informations sont enregistrer lorsque l'on revient sur la page créer dossier.</p>
+        <img src="">Image remplissage info client</img>
+        
+    </div>
+   
+    <div id="doc_transferer_client" class="contenu_doc">
+        <p>
+        La modification d'un dossier créé par un autre utilisateur n'est pas possible. Pour pouvoir le modifier il faut que le 
+        proprietaire du dossier indique que le dossier appartient maintenant à un autre utilisateur.</p> 
+        <p>Pour cela le proprietaire du dossier doit aller sur la page d'edition du dossier puis choisir l'utilisateur qu'il veut nommer 
+            proprietaire.</p>        
+        <img src="">Image remplissage info client</img>
+        <p>Cliquer ensuite sur le bouton "Modifier dossier" pour enregistrer les modification apporté au dossier.</p>
+        
+    </div>
      <?php
             if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
             ?>
@@ -107,9 +127,27 @@
      <?php } ?>
      
      <!--DOC GERER FIHCIERS-->
-    <div id="doc_stocker_fichier" class="contenu_doc">CONTENU PAGE STOCKER FICHIER</div>
-    <div id="doc_supprimer_fichier" class="contenu_doc">CONTENU PAGE SUPPRIMER FICHIER</div>
-    <div id="doc_telecharger_fichier" class="contenu_doc">CONTENU PAGE TELECHARGER FICHIER</div>
+    <div id="doc_stocker_fichier" class="contenu_doc">
+        <p>L'application offre la possibilité de joindre des fichier (word, pdf, ...) au dossier.</p>
+        <p>Pour cela rendez-vous sur le dossier ou vous voulez ajouter un fichier. Ensuite dans l'espace 
+            "Gestion fichiers" cliquer sur le bouton "Ajouter fichier...", vous êtes redirigé vers une page 
+            ou l'on vous demande de choisir un fichier. Cliquez sur le bouton "Choisir un fichier" puis 
+            selectionner le fichier à ajouter se trouvant sur votre ordinateur.<br>
+        Cliquez ensuite sur le bouton "Valider", vous êtes redirigé vers le dossier précédemment afficher où le fichier ajouté précédemment est affiché à présent.</p>
+    </div>
+    <div id="doc_supprimer_fichier" class="contenu_doc">
+        <p>Vous pouvez supprimer les fichiers qui ont été joints par erreur.</p>
+        <p>Pour cela rendez-vous sur le dossier ou un fichier à été ajouté. Ensuite dans l'espace 
+            "Gestion fichiers" cliquer sur l'icone "sens-interdit" permettant de supprimer le fichier, 
+            une fenêtre s'ouvre vous demandant de valider la suppression cliquer sur oui. La fenêtre disparait et le fichier est bien supprimé</p>
+            
+    </div>
+    <div id="doc_telecharger_fichier" class="contenu_doc">
+        <p>Vous pouvez télécharger les fichiers ajoutés au dossier.</p>
+        <p>Pour cela rendez-vous sur le dossier ou un fichier à été ajouté. Ensuite dans l'espace 
+            "Gestion fichiers" cliquer sur le nom du fichier afin de télécharger le fichier voulu</p>
+            
+    </div>
     
     <!--DOC GERER FOURNISSEURS-->
     <div id="doc_ajouter_fournisseur" class="contenu_doc">CONTENU PAGE AJOUTER FOURNISSEUR</div>
