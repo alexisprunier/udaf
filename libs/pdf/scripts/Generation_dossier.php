ddf7c1b3f942558963de4e33bf3f9448858e631a
alexisprunier <prunier.al@gmail.com>
2013-06-24 14:51:37 +0200

    Génération PDF rempli

--- libs/pdf/scripts/Generation_dossier.php
+++ libs/pdf/scripts/Generation_dossier.php
@@ -1,7 +1,6 @@
 <?php
-
+$NB_EVENEMENTS = 3;
 // Génération du dossier en pdf
-
 require_once(CHEMIN_LIB.'pdf/phpToPDF.php');
 require_once(CHEMIN_LIB.'pdf/fpdf.php');
 require_once(CHEMIN_LIB.'pdf/pdfclass.php');
@@ -12,67 +11,96 @@ $pdf = new PDF();
 
 $pdf->SetDrawColor(200 , 200, 200);
 $pdf->AddPage('');
+
 $pdf->Image('images/logo/LogoUDAF_fond_blanc.png', 30, 100, 150);
 
 $pdf->SetFont('arial','B',14);
-$pdf->SetXY(20, 50);
+$pdf->SetXY(20, 40);
 $pdf->MultiCell(170,5,html_entity_decode(utf8_decode('
-      Informations du Dossier :
+      Clôture du dossier :
 ')),0);
 $pdf->SetFont('arial','B',10);
-$pdf->SetXY(20, 60);
-$pdf->MultiCell(80,7,html_entity_decode(utf8_decode('
-  Propriétaire :
-  Thème :
-  Sous-thème :
-  Commentaire :
+$pdf->SetXY(20, 50);
+$pdf->MultiCell(170,7,html_entity_decode(utf8_decode('
+    Cloturé :
+    Raison de clôture :
+    Date de clôture :
+    Commentaire :  
   
 
 
 ')),1);
+//Pointilés info cloture
 $pdf->SetFont('arial','',10);
-$pdf->SetXY(50, 60);
-$pdf->MultiCell(80,7,html_entity_decode(utf8_decode('
-  '.$utilisateur['prenom'].' '.$utilisateur['nom'].'
-  '.$theme['nom'].'
-  '.$sstheme['nom'].'
-')),0);
-$pdf->SetFont('arial','',10);
-$pdf->SetXY(25, 95);
-$pdf->MultiCell(70,5,html_entity_decode(utf8_decode($dossier_select['problematique']
-)),0);
-$pdf->SetFont('arial','B',10);
-$pdf->SetXY(110, 60);
-$pdf->MultiCell(80,7,html_entity_decode(utf8_decode('
-  Cloturé :
-  Raison de clôture :
-  Date de clôture :
-  Commentaire :
-  
-
-
-')),1);
-$is_cloture = $dossier_select['cloture'] == 1 ? "Oui" : "Non";
-$pdf->SetFont('arial','',10);
-$pdf->SetXY(150, 60);
-$pdf->MultiCell(80,7,html_entity_decode(utf8_decode('
+$pdf->SetXY(65, 50);
+$pdf->MultiCell(170,7,html_entity_decode(utf8_decode('
   '.$is_cloture.'
   '.$dossier_select['raison_cloture'].'
   '.$dossier_select['date_cloture'].'
 ')),0);
+//Pointilés commentaire de cloture
 $pdf->SetFont('arial','',10);
-$pdf->SetXY(115, 95);
-$pdf->MultiCell(70,5,html_entity_decode(utf8_decode($dossier_select['comment_cloture']
+$pdf->SetXY(20, 76);
+$pdf->MultiCell(170,7,html_entity_decode(utf8_decode(
+$dossier_select['comment_cloture']
 )),0);
+
 $pdf->SetFont('arial','B',14);
-$pdf->SetXY(20, 120);
+$pdf->SetXY(20, 105);
+$pdf->MultiCell(170,5,html_entity_decode(utf8_decode('
+      Informations du Dossier :
+')),0);
+$pdf->SetFont('arial','B',10);
+$pdf->SetXY(20, 115);
+$pdf->MultiCell(170,7,html_entity_decode(utf8_decode('
+  Propriétaire :
+  Thème :
+  Sous-thème :
+  Problématique :
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+')),1);
+$pdf->SetFont('arial','',10);
+$pdf->SetXY(55, 115);
+$pdf->MultiCell(150,7,html_entity_decode(utf8_decode('
+  '.$utilisateur['prenom'].' '.$utilisateur['nom'].'
+  '.$theme['nom'].'
+  '.$sstheme['nom'].'
+')),0);
+//Pointillés problématique
+$pdf->SetFont('arial','',10);
+$pdf->SetXY(25, 151);
+$pdf->MultiCell(170,7,html_entity_decode(utf8_decode(
+    $dossier_select['problematique']
+)),0);
+$pdf->AddPage('');
+$pdf->Image('images/logo/LogoUDAF_fond_blanc.png', 30, 100, 150);
+
+$pdf->SetFont('arial','B',14);
+$pdf->SetXY(20, 40);
 $pdf->MultiCell(160,5,html_entity_decode(utf8_decode('
       Informations du Client :
   
 ')),0);
 $pdf->SetFont('arial','B',12);
-$pdf->SetXY(20, 130);
-$pdf->MultiCell(170,6,html_entity_decode(utf8_decode('
+$pdf->SetXY(20, 50);
+$pdf->MultiCell(170,7,html_entity_decode(utf8_decode('
   Nom :
   Prénom :
   Adresse :
@@ -84,8 +112,8 @@ $pdf->MultiCell(170,6,html_entity_decode(utf8_decode('
 
 ')),1);
 $pdf->SetFont('arial','',12);
-$pdf->SetXY(100, 130);
-$pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
+$pdf->SetXY(60, 50);
+$pdf->MultiCell(150,7,html_entity_decode(utf8_decode('
   '.$client['nom'].'
   '.$client['prenom'].'
   '.$client['adr_postale'].'
@@ -94,16 +122,16 @@ $pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
   '.$client['mail'].'
   '.$client['tel_fixe'].'
   '.$client['tel_port'].'
-
 ')),0);
+
 $pdf->SetFont('arial','B',14);
-$pdf->SetXY(20, 190);
+$pdf->SetXY(20, 120);
 $pdf->MultiCell(160,5,html_entity_decode(utf8_decode('
       Informations du Fournisseur :
   
 ')),0);
 $pdf->SetFont('arial','B',12);
-$pdf->SetXY(20, 200);
+$pdf->SetXY(20, 130);
 $pdf->MultiCell(170,6,html_entity_decode(utf8_decode('
   Nom :
   Prénom :
@@ -117,8 +145,8 @@ $pdf->MultiCell(170,6,html_entity_decode(utf8_decode('
 
 ')),1);
 $pdf->SetFont('arial','',12);
-$pdf->SetXY(100, 200);
-$pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
+$pdf->SetXY(60, 130);
+$pdf->MultiCell(150,6,html_entity_decode(utf8_decode('
   '.$fournisseur['nom'].'
   '.$fournisseur['prenom'].'
   '.$fournisseur['raison_sociale'].'
@@ -131,15 +159,13 @@ $pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
   '.$fournisseur['comment_fournisseur'].'
 ')),0);
 
-$pdf->AddPage('');
-$pdf->Image('images/logo/LogoUDAF_fond_blanc.png', 30, 100, 150);
 $pdf->SetFont('arial','B',14);
-$pdf->SetXY(20, 50);
+$pdf->SetXY(20, 200);
 $pdf->MultiCell(80,5,html_entity_decode(utf8_decode('
       Fichiers :
 ')),0);
 $pdf->SetFont('arial','',10);
-$pdf->SetXY(20, 60);
+$pdf->SetXY(20, 210);
 $pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
   Fichier 1 :
   Fichier 2 :
@@ -149,22 +175,22 @@ $pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
   Fichier 6 :
 
 ')),1);
-$hauteur_fichier = 66;
+$hauteur_fichier = 216;
 foreach ($tab_fichier as &$fichier) {
     if ($fichier->dossier_id == $_SESSION['dossier_ref']) {
         $pdf->SetFont('arial','',10);
-        $pdf->SetXY(55, $hauteur_fichier);
+        $pdf->SetXY(40, $hauteur_fichier);
         $hauteur_fichier += 6;
         $pdf->MultiCell(80,6,html_entity_decode(utf8_decode($fichier->nom)),0);
     }
 }
 $pdf->SetFont('arial','B',14);
-$pdf->SetXY(110, 50);
+$pdf->SetXY(110, 200);
 $pdf->MultiCell(80,5,html_entity_decode(utf8_decode('
       Sites web :
 ')),0);
 $pdf->SetFont('arial','',10);
-$pdf->SetXY(110, 60);
+$pdf->SetXY(110, 210);
 $pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
   Nom site 1 :
   Lien site 1 :
@@ -174,7 +200,7 @@ $pdf->MultiCell(80,6,html_entity_decode(utf8_decode('
   Lien site 3 :
 
 ')),1);
-$hauteur_fichier = 66;
+$hauteur_fichier = 216;
 $i = 0;
 foreach ($tab_site as &$site) {
     if ($site->dossier_id == $_SESSION['dossier_ref'] && i < 3) {
@@ -187,46 +213,67 @@ foreach ($tab_site as &$site) {
         $i += 1;
     }
 }
-$pdf->SetFont('arial','B',14);
-$pdf->SetXY(20, 110);
-$pdf->MultiCell(80,5,html_entity_decode(utf8_decode('
-      Evénements :
-')),0);
-$hauteur_evenement = 120;
 foreach ($tab_evenement as &$evenement) {
     if ($evenement->dossier_id == $_SESSION['dossier_ref']) {
         $is_traite = $evenement->traite == 1 ? "Oui" : "Non";
         $user_ev = selectionner_utilisateur_dans_bdd($evenement->user_id);
-        $pdf->SetFont('arial','B',10);
-        $pdf->SetXY(20, $hauteur_evenement);
-        $pdf->MultiCell(170,5,html_entity_decode(utf8_decode('
-           Date :
-           Utilisateur :
-           Mode de contact :
-           Traité :
-           Commentaire :
-        ')),1);
-        $pdf->SetFont('arial','',10);
-        $pdf->SetXY(60, $hauteur_evenement);
-        $pdf->MultiCell(170,5,html_entity_decode(utf8_decode('
-           '.$evenement->date_event.'
-           '.$user_ev['nom'].' '.$user_ev['prenom'].'
-           '.$evenement->mode_contact.'
-           '.$is_traite.'
-           '.$evenement->comm_event.'
+
+        $pdf->AddPage('');
+        $pdf->Image('images/logo/LogoUDAF_fond_blanc.png', 30, 100, 150);
+
+        $pdf->SetFont('arial','B',14);
+        $pdf->SetXY(20, 40);
+        $pdf->MultiCell(80,5,html_entity_decode(utf8_decode('
+              Evénements :
         ')),0);
-        if ( $hauteur_evenement >= 200){
-            $pdf->AddPage('');
-            $pdf->Image('images/logo/LogoUDAF_fond_blanc.png', 30, 100, 150);
-            $hauteur_evenement = 60;
-        }else{
-            $hauteur_evenement += 45;
-        }
+
+
+            $pdf->SetFont('arial','',12);
+            $pdf->SetXY(20, 50);
+            $pdf->MultiCell(170,7,html_entity_decode(utf8_decode('
+          Date :
+          Utilisateur :
+          Mode de contact :
+          Traité :
+          Commentaire :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ')),1);
+            $pdf->SetFont('arial','',12);
+            $pdf->SetXY(53, 50);
+            $pdf->MultiCell(150,7,html_entity_decode(utf8_decode('
+                '.$evenement->date_event.'
+                '.$user_ev['nom'].' '.$user_ev['prenom'].'
+                '.$evenement->mode_contact.'
+                '.$is_traite.'
+            ')),0);
+            $pdf->SetFont('arial','',12);
+            $pdf->SetXY(25, 95);
+            $pdf->MultiCell(175,7,html_entity_decode(utf8_decode($evenement->comm_event)),0);
     }
 }
-
-
-
-
 $pdf->Output($path);
 ?>
