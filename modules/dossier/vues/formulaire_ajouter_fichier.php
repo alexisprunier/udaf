2b81b2327e60be95970e7f3dd321ce802631145e
tonyodelaliverda <tonyodelaliverda@bitbucket.org>
2013-06-24 18:34:19 +0200

    corrections dossier ref

--- modules/dossier/vues/formulaire_ajouter_fichier.php
+++ modules/dossier/vues/formulaire_ajouter_fichier.php
@@ -5,11 +5,11 @@
 
 <div id="ajouter_fichier">
   <h3> Ajouter fichier au dossier <?php echo $_SESSION['dossier_ref']; ?></h3>
-  <form name = "form_fichier" action="accueil.php?module=dossier&action=creer_dossier&ajout=fichiers&id=<?php echo $_SESSION['dossier_ref']; ?>" method="post" enctype="multipart/form-data">
+  <form name = "form_fichier" action="accueil.php?module=dossier&action=creer_dossier&ajout=fichiers&id=<?php echo $_GET['id']; ?>" method="post" enctype="multipart/form-data">
     <fieldset class="ogconso">
         <!--<input type="hidden" name="MAX_FILE_SIZE" value="5000000"> <!-- 5Mo-->
         <input id="fichier" name="fichier" type="file" required /><br />
-        
+        <input type="button" value="Retour" onclick="location.href='/accueil.php?module=dossier&action=creer_dossier&id='+<?php echo $_SESSION['dossier_ref']; ?>+'&from=fichier'"/>
         <button>Envoyer</button>
     </fieldset>
   </form>
