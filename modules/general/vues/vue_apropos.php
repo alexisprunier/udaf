0d73e05a7998eef3abcf0af428fd045a0588a577
tonyodelaliverda <tonyodelaliverda@bitbucket.org>
2013-06-27 10:04:05 +0200

    a propos

--- modules/general/vues/vue_apropos.php
+++ modules/general/vues/vue_apropos.php
@@ -2,3 +2,4 @@
 
 include CHEMIN_VUE_GLOBALE . 'aucun_contenu.php';
 ?>
+Cette application à été développée par des élèves de l'Institut d'Informatique Appliquée de Laval sous le contrôle de Michel Thomas et Jean Paul Gastinel.
